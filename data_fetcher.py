9621ce762e2c4009cf0bff64895b5a3cd729385e
OluwaseyiPopoola <oluwaseyipopoola49@gmail.com@[xula.edu]>
2026-03-15 20:09:32 +0000

    Refactor get_user_workouts to allow for querying

--- data_fetcher.py
+++ data_fetcher.py
@@ -74,26 +74,43 @@ def get_user_workouts(user_id):
 
     This function currently returns random data. You will re-write it in Unit 3.
     """
+    client = bigquery.Client()
+    query = """
+        SELECT
+            WorkoutId,
+            StartTimestamp,
+            EndTimestamp,
+            StartLocationLat,
+            StartLocationLong,
+            EndLocationLat,
+            EndLocationLong,
+            TotalDistance,
+            TotalSteps,
+            CaloriesBurned
+        FROM `jesus-munoz-utep.ISE.Workouts`
+        WHERE UserId = @UserId
+        """
+            
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("UserId", "STRING", user_id)
+        ])
+
+    results = client.query(query, job_config=job_config).result()
+
     workouts = []
-    for index in range(random.randint(1, 3)):
-        random_lat_lng_1 = (
-            1 + random.randint(0, 100) / 100,
-            4 + random.randint(0, 100) / 100,
-        )
-        random_lat_lng_2 = (
-            1 + random.randint(0, 100) / 100,
-            4 + random.randint(0, 100) / 100,
-        )
+    for row in results:
         workouts.append({
-            'workout_id': f'workout{index}',
-            'start_timestamp': '2024-01-01 00:00:00',
-            'end_timestamp': '2024-01-01 00:30:00',
-            'start_lat_lng': random_lat_lng_1,
-            'end_lat_lng': random_lat_lng_2,
-            'distance': random.randint(0, 200) / 10.0,
-            'steps': random.randint(0, 20000),
-            'calories_burned': random.randint(0, 100),
+            'workout_id': row.WorkoutId,
+            'start_timestamp': row.StartTimestamp.strftime('%Y-%m-%d %H:%M:%S'),
+            'end_timestamp': row.EndTimestamp.strftime('%Y-%m-%d %H:%M:%S'),  
+            'start_lat_lng': (row.StartLocationLat, row.StartLocationLong),
+            'end_lat_lng': (row.EndLocationLat, row.EndLocationLong),
+            'distance': row.TotalDistance,
+            'steps': row.TotalSteps,
+            'calories_burned': int(row.CaloriesBurned),
         })
+
     return workouts
 
 
